4df46ac4bcfaf0391718a0d03483818338af3d23
Rohinish048 <145529581+Rohinish048@users.noreply.github.com>
2024-11-24 22:54:36 +0530

    Commited by me

---
+++ 1.py
@@ -0,0 +1,4 @@
+import os
+from ipynbcompress import compress
+filename = 'C:\\Users\\rohin\\OneDrive\\Desktop\\YOLOv8_Traffic_Density_Estimation-master\\YOLOv8_Traffic_Density_Estimation-master\\jupyter_file.ipynb'
+out = 'C:\\Users\\rohin\\OneDrive\\Desktopcompressed.ipynb'
